1ea0c8c9a3b52c7bdeeca98a1be0cc624c31d371
Sourabh Kumar Singh <sour.bt21cs56@opju.ac.in>
2024-03-03 13:29:39 +0530

    🐛fixex: routes ✨feat: post, update & delete author endpoints

--- src/utils/db.server.ts
+++ src/utils/db.server.ts
@@ -1,20 +1,20 @@
 // Defining the prism client
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 // Defining the db variable to be used in the application to access the database client instance of type PrismaClient
-let db: PrismaClient
+let db: PrismaClient;
 
 // Defining the global variable to store the database client instance
 declare global {
-    var _db: PrismaClient | undefined
+  var __db: PrismaClient | undefined;
 }
 
 // Checking if the global variable is not defined, then create a new instance of PrismaClient and assign it to the global variable
-if (!global._db) {
-    global._db = new PrismaClient()
+if (!global.__db) {
+  global.__db = new PrismaClient();
 }
 
 // Assigning the global variable to the db variable
-db = global._db
+db = global.__db;
 
-export {db}
+export { db };
